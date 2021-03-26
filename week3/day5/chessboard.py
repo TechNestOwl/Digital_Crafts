4e06ce0e677191c03d981608632d6b6d8f09f434
Gill Guimaraes <technestowl@gmail.com>
2021-03-26 12:44:07 -0400

    added day5

---
+++ week3/day5/chessboard.py
@@ -0,0 +1,14 @@
+# def chessBoard():
+
+
+def showChessBoard():
+    # row =
+    # col =
+
+    Squares = {black: 0, white: 1}
+
+    for square in Squares:
+        print(square)
+
+
+showChessBoard()
