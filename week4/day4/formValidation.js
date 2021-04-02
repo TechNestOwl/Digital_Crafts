79b77be2762a1bc9047767a044d0735d8e238275
Gill Guimaraes <technestowl@gmail.com>
2021-04-01 20:58:30 -0400

    revised JsCalc

--- week4/day4/formValidation.js
+++ week4/day4/formValidation.js
@@ -1,21 +1,64 @@
+
 const form = document.querySelector("form");
-const email = document.querySelector("email").value
 
-const validateForm = (email, password) => {
-    console.log("valadating..");
+const validateNumbers = (number) => {
+    myRegex = /^(0|[1-9]\d*)?(\.\d+)?(?<=\d)$/;
+    return myRegex.test(number);
+};
 
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value))
+form.addEventListener("submit",(event) => {
+    const billInput = document.querySelector("#bill");
+    const tipInput = document.querySelector("#tip");
     
-    return (true)
-        alert("You have entered an invalid email address!")
-    return (false)
+    if(
+        !validateNumbers(billInput.value) ||
+        !validateNumbers(tipInput.value)
+        ){
+            event.preventDefault();
+            alert("Please enter valid amount.");
+        } else {
+            event.preventDefault();
+            calculateTip();
+        }
+});
     
-}
+    function calculateTip() {
+
+        const billAmount = document.querySelector("#bill").value;
+        //console.log(billAmount)
+        const tipAmount = document.querySelector("#tip").value;
+        //console.log(tipAmount)
+        //Calculate
+        const tipValue = billAmount * `1.${tipAmount}`;
+        const yourTip = document.querySelector(".total");
+        yourTip.innerText = tipValue.toFixed(2);
+
+        //create tip history List
+        /*const tipDiv = document.createElement("div")
+        tipDiv.className = "tip-history"*/
+        //console.log(tipDiv)
+        const list = document.querySelector(".tip-history");
+        const newListItem = document.createElement("li");
+        newListItem.innerText = `${tipAmount}%`;
+        list.append(newListItem);
+    }
+    // const validateNumber = () => {
+        //     console.log("validate")
+        //     myRegex = /[0-9]+$/;
+        //     return myRegex.test(number);
+        
+        
+// }
+
+// form.addEventListener("submit", (event)) => {
+//     const numInput = document.querySelector("#bill").value
+
+// }
 
 
-
-
-
-
-
-
+/* 
+if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)
+return (true)
+    alert("You have entered an invalid email address!")
+return (false)
+*/
