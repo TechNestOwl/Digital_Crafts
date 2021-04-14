e43a8418c8daa93e4127964840ead49caddaaad2
Gill Guimaraes <technestowl@gmail.com>
2021-04-14 14:04:32 -0400

    major styling change to book1 link

--- week6/day1/scrivScript.js
+++ week6/day1/scrivScript.js
@@ -1,9 +1,11 @@
-const mainContainer = document.querySelector("#main-container");
+const pubInfo = document.querySelector("#publishingInfo");
 const bookTitle = document.querySelector("#bookTitle");
 const publishDate = document.querySelector("#publishDate");
-const bookDescription = document.querySelector("#bookDescription")
+const bookDescription = document.querySelector("#bookDescription");
+const fileContent = document.querySelector(".fileContent");
+const infoBtn = document.querySelector("#infoBtn");
 
-const getBook = async () => {
+const getBookGatsby = async () => {
     const retrieveBook = await fetch("https://openlibrary.org/books/OL26491056M.json")
     let formattedBook = await retrieveBook.json();
     console.log(formattedBook);
@@ -14,5 +16,20 @@ const getBook = async () => {
     const bookDescrip = formattedBook.description;
     bookDescription.innerHTML = bookDescrip;
     // console.log(formattedGatsby.description);
+
 }
-getBook();
+infoBtn.addEventListener("click", function() {
+    getBookGatsby();
+})
+
+// get text stats for selected book
+
+function getDocStats(txtFile) {
+    let docLength = document.getElementById("docLength");
+    let wordCount = document.getElementById("wordCount");
+    let charCount = document.getElementById("charCount");
+
+    let text = txtFile;
+    console.log(txtFile);
+}
+getDocStats(fileContent);
