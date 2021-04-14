7e38b94d74604821b0eab40f6804a1ec2deebed1
Gill Guimaraes <technestowl@gmail.com>
2021-04-14 10:13:47 -0400

    day startup

--- week6/day1/book2.js
+++ week6/day1/book2.js
@@ -4,15 +4,15 @@ const publishDate = document.querySelector("#publishDate");
 const bookDescription = document.querySelector("#bookDescription")
 
 const getBook = async () => {
-    const retrieveBook = await fetch("https://openlibrary.org/books/OL26491056M.json")
+    const retrieveBook = await fetch("https://openlibrary.org/books/OL7173600M.json")
     let formattedBook = await retrieveBook.json();
     console.log(formattedBook);
 
     const bookPublishDate = formattedBook.publish_date;
     publishDate.innerHTML = bookPublishDate;
     
-    const bookDescrip = formattedBook.description;
-    bookDescription.innerHTML = bookDescrip;
+    // const bookDescrip = formattedBook.first_sentence.value;
+    // bookDescription.innerHTML = bookDescrip;
     // console.log(formattedGatsby.description);
 }
 getBook();
