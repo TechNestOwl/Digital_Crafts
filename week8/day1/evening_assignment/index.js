2ed9c873f6823aee118d1f9c10fcffc417d7bf47
Gill Guimaraes <technestowl@gmail.com>
2021-04-26 15:03:09 -0400

    adding express templates

---
+++ week8/day1/evening_assignment/index.js
@@ -0,0 +1,20 @@
+const PORT = 3001;
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html',es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+const pool = require("./db.js");
+
+
+app.get("/", (req,res)=> {
+    res.send("Welcome to Node server")
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on port:${PORT}`)
+});
