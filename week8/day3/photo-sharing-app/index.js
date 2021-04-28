1eb575611594f505db9bf4b2147eaa948d31a3b8
Gill Guimaraes <technestowl@gmail.com>
2021-04-28 16:50:24 -0400

    sequelize ORM progress save

--- week8/day3/photo-sharing-app/index.js
+++ week8/day3/photo-sharing-app/index.js
@@ -18,6 +18,45 @@ app.post('/users', async (req,res)=> {
 
 //retrieve a user
 app.get('/users', async (req,res) => {
-    const users = await User.findAll();
-    res.json(users);
+    //'.findAll()  - selects all columns by defualt. Provide an Attributs option to specify column names:
+    try{
+        const users = await User.findAll({
+            attributes: ['lastName']
+        });
+        res.json(users);
+    }catch (e) {
+        console.log(e);
+        res.status(404).json({
+            message: "User not found"
+        });
+    }
+});
+// to retrieve by ID use ' .findByPk() ' find by primary key
+//to do a TEXT search - use 'whree'
+    // const users = await User.findAll({
+    //     where: {
+    //         firstName: req.body.term,
+    //     }
+    // });
+
+//Updating Existing Users note: it's best to use where option so that you don't update all rows.
+app.post('/users/:id', async (req,res) => {
+    const { id } = req.params;
+    //assuming that 'req.body' is limited to 
+    //the keys firstName, lastName, email
+    const updateUser = await User.update(req.body, {
+        where:{
+            id
+        }
+    });
+    res.json(updatedUser);
+});
+
+//deleting a user
+app.delete('/users/:id', async (req,res) => {
+    const {id} = req.params;
+    const deletedUser = await User.destroy({
+        whrere: {id}
+    });
+    res.json(deletedUser);
 });
